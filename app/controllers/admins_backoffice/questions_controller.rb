24c27dc31194d0c0798c55ce0ea9c9aa10a05dcf
Jackson Pires <jack@jack.com>
2022-02-20 23:59:33 +0000

    created views for question model, implemented i18n into models, created a new task for answers, installed and configured cocoon, configured a template for user and admin devise and installed Rails DB

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,8 +1,11 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:new, :edit] 
    
   def index
-    @questions = Question.all.order(:description).page(params[:page])
+    @questions = Question.includes(:subject)
+                         .order(:description)
+                         .page(params[:page])
   end
 
   def new
@@ -12,7 +15,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   def create
     @question = Question.new(params_question)
     if @question.save
-      redirect_to admins_backoffice_question_path, notice: "Questão cadastrada com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão cadastrada com sucesso!"
     else
       render :new
     end
@@ -23,7 +26,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def update
     if @question.update(params_question)
-      redirect_to admins_backoffice_question_path, notice: "Questão atualizada com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão atualizada com sucesso!"
     else
       render :edit
     end
@@ -31,7 +34,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   
   def destroy
     if @question.destroy
-      redirect_to admins_backoffice_question_path, notice: "Questão excluída com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
     else
       render :index
     end
@@ -39,10 +42,15 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   private
 
   def params_question
-    params.require(:question).permit(:description, :subject_id)
+    params.require(:question).permit(:description, :subject_id,
+       answers_attributes: [:id, :description, :correct, :_destroy])
   end
   
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def get_subjects
+    @subjects = Subject.all
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module TimeToAnswer
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.web_console.whitelisted_ips = '10.0.2.2'
   end
 end
