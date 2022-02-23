725bd0b025b57d957c44d93c42fc1a738f6b1757
nodMarcos <nodmarcos@gmail.com>
2022-02-23 17:54:07 +0000

    added birthdate to the user profile edit, a modal to change de users photos, made some changes to the UX, made the user and admin index show statistics of his performance, made the reset password work and added a action mailer to the project, to send email when an admin change your info

---
+++ time_to_answer/app/models/question.rb
@@ -0,0 +1,35 @@
+class Question < ApplicationRecord
+  belongs_to :subject, counter_cache: true, inverse_of: :questions
+  has_many :answers
+  accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  #Kaminari
+  paginates_per 5
+
+  after_create :set_statistic
+
+  #   Scopes
+  scope :_search_, -> (page, term) {
+    includes(:answers, :subject)
+    .where("lower(description) LIKE ?", "%#{term.downcase}%")
+    .page(page)
+  }
+  
+  scope :_search_subject_, -> (page, subject_id) {
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
+
+  scope :last_questions, -> (page) {
+    includes(:answers, :subject)
+    .order('created_at desc')
+    .page(page)
+  }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+  end
+end
